3076fbcd7e251919d240b97ebe8e1d78cf03615c
BravoClassic <gerald.akorli@gmail.com>
2023-09-30 00:25:37 -0600

    done with project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import { FlashCards } from './components/FlashCards';
+import  { useState } from 'react';
+import {data,colors}  from './data.js';
 
 function App() {
-
+  const [currentCard, setCurrentCard] = useState(-1);
+  const [currentDeck, setCurrentDeck] = useState("Europe");
+  const keys = Object.keys(data);
+  const changeDeck = () => {
+    setCurrentDeck(keys[Math.floor(Math.random()*keys.length)]);
+    setCurrentCard(Math.floor(Math.random()*data[currentDeck].length));
+  }
   return (
     <>
-      <p>Gerlad</p>
+     <h1>FlashCards - Learning Geography </h1>
+     <p>Learn countries and their capitals through these flash cards</p>
+     <FlashCards  data={data[currentDeck][currentCard]} background={colors[currentDeck]} currentDeck={currentDeck} currentCard={currentCard} />
+     <button onClick={changeDeck}>Next</button>
     </>
-  )``
+  )
 }
-
-export default App
+export default App;
